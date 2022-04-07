1fee43940ba5ab33bba178def87a02f4b522d48f
marioacar <mcarvantes@ucol.mx>
2022-04-06 23:34:56 -0500

    Agregar archivos de proyecto.

---
+++ validacion de formulario/validacion de formulario/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using validacion_de_formulario.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace validacion_de_formulario
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Formulario());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
