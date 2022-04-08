6e6f2e674dc050532795dbb86f582f8f6164deac
marioacar <mcarvantes@ucol.mx>
2022-04-07 23:38:35 -0500

    sadasdasd

---
+++ validacion de formulario/validacion de formulario/Model/form.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace validacion_de_formulario.Model
+{
+    internal class form
+    {
+    }
+}
---
+++ validacion de formulario/validacion de formulario/ViewModel/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace validacion_de_formulario.ViewModel
+{
+    internal class BaseViewModel
+    {
+    }
+}
